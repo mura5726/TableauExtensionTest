cc0a1459d6eb4f23b519be1d00c2d31c1b3858b3
MMS <mura5726@gmail.com>
2019-08-26 11:47:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ tableau.extensions.initializeAsync().then(() => {
 });
 
 function SendToTableau() {
+  target = document.getElementById("output");
+  target.innerText = document.forms.form1.textBox1.value;
+  
   let dashboard = tableau.extensions.dashboardContent.dashboard;
   // ここに何を書けばよいかわからない。。
 }
