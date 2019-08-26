41afb0fc5406750d7a98e5f6cc9e447937038c08
MMS <mura5726@gmail.com>
2019-08-26 17:24:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ tableau.extensions.initializeAsync().then(() => {
   console.log('I have been initialized!!')
 });
 
+function clickBtn1(){
+  const t1 = document.form1.text1.value;
+  document.getElementById("span1").textContent = t1;
+}
+function clickBtn2(){
+  document.getElementById("span1").textContent = "";
+}
+
 function SendToTableau() {
   target = document.getElementById("output");
   target.innerText = document.forms.form1.textBox1.value;
